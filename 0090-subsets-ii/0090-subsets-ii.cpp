04c6e471c163a8415186a420d5f4dc7598dd08a3
Maithli Arora <connectwithmaithli@gmail.com>
2026-08-05 14:15:03 +0530

    Time: 3 ms (31.47%), Space: 10.3 MB (85.19%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void helper(int idx,vector<int>& nums, vector<vector<int>>&ans,vector<int>&ds){
+        ans.push_back(ds);
+        for(int i=idx;i<nums.size();i++){
+            if(i!=idx&&nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            helper(i+1,nums,ans,ds);
+            ds.pop_back();
+    }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        helper(0,nums,ans,ds);
+        return ans;
+
+        
+    }
+};
