f0db2617885c947d2c9e6c5f88b2b2f0abce212c
Maithli Arora <connectwithmaithli@gmail.com>
2026-08-20 09:31:28 +0530

    Time: 0 ms (100%), Space: 23.7 MB (83.63%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.cpp
@@ -10,16 +10,19 @@ public:
         int j=0;
         int k=0;
         for(int i=2;i<n;i++){
-            if(a1.back()>a2.back()){
+            if(a1[j]>a2[k]){
                 a1.push_back(nums[i]);
+                j++;
             }
             else{
                 a2.push_back(nums[i]);
+                k++;
             }
         }
         for(int i=0;i<a2.size();i++){
             a1.push_back(a2[i]);
         }
         return a1;
+
     }
 };
